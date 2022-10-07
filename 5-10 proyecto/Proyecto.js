e191c2ec79b832d007507977f69a802e72fd22eb
jzarzosa <majorminus4697@gmail.com>
2022-10-07 11:58:49 -0300

    admin, inputs y tabla

--- 5-10 proyecto/Proyecto.js
+++ 5-10 proyecto/Proyecto.js
@@ -1,10 +1,11 @@
 let buttonSave = document.getElementById('buttonSave');
 let title = document.getElementById('title');
-let categoria = document.getElementById('categoria');
-let descripcion = document.getElementById('descripcion');
+let category = document.getElementById('categoria');
+let description = document.getElementById('descripcion');
 let url = document.getElementById('url');
 let carrusel = document.getElementById('carrusel');
 let fila = document.getElementById('fila');
+
 
 
 ReadFunction();
@@ -13,19 +14,20 @@ let pelicula = JSON.parse(localStorage.getItem('Peliculas')) || [];
 
 buttonSave.addEventListener('click', () => {
 
-    if (title.value != "" && categoria.value != "") {
+    if (title.value != "" && description.value != "" && category.value != "") {
         pelicula.push({
+
             id: Math.round(Math.random() * 100000),
             title: title.value,
-            categoria: categoria.value,
-            descripcion: descripcion.value,
+            category: categoria.value,
+            description: descripcion.value,
             url: url.value,
 
         })
         localStorage.setItem('Peliculas', JSON.stringify(pelicula));
         title.value = "";
-        categoria.value = "";
-        descripcion.value = "";
+        category.value = "";
+        description.value = "";
         url.value = "";
 
         readFunction();
@@ -43,14 +45,13 @@ function ReadFunction() {
 
     if (getLocalStorage != null) {
         for (let index = 0; index < getLocalStorage.length; index++) {
-            arrayPeliculas.push(`
-            
+            arrayPeliculas.push( `
             <tr>
-            <th scope="row">${(index + 1)}</th>
+            <th scope="row">${(index + 3)}</th>
             
             <td>${getLocalStorage[index].title}</td>
-            <td>${getLocalStorage[index].categoria}</td>
-            <td>${getLocalStorage[index].descripcion}</td>
+            <td>${getLocalStorage[index].category}</td>
+            <td>${getLocalStorage[index].description}</td>
             <td>${getLocalStorage[index].url}</td>
             <td><button class="delete btn btn-white"><img src="/images/eliminar.png" alt=""></button></td>
             <td><button class="add btn btn-white" onclick="addMovieCarousel()"><img src="/images/mas.png" alt=""></button></td>
@@ -73,20 +74,29 @@ function addMovieCarousel() {
 
     let getLocalStorage = JSON.parse(localStorage.getItem("Peliculas"));
 
-    
-        for (let index = 0; index < getLocalStorage.length; index++) {
-            carousel.push(`
-            
-            <div class="carousel-item active">
-            <img src="${getLocalStorage[index].url}" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>${getLocalStorage[index].title}</h5>
-                <p><td>${getLocalStorage[index].categoria}</td></p>
+
+    for (let index = 0; index < getLocalStorage.length; index++) {
+        carousel.push(
+            `
+            <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="${index}"
+                class="active" aria-current="true" aria-label="Slide ${[index + 1]}"></button>
+        </div>
+        <div class="carousel-inner">
+
+
+            <div class="carousel-item">
+                <img src="${getLocalStorage[index].url}" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>${getLocalStorage[index].title}</h5>
+                    <p>${getLocalStorage[index].category}</p>
+                </div>
             </div>
-        </div> 
+
+        </div>
                         `)
-        }
-
-
-        carrusel.innerHTML = carousel
     }
+
+    carrusel.innerHTML = carousel
+}
+ 
