718baee52f60acf5ab743e7613881500d47ddcaa
jzarzosa <majorminus4697@gmail.com>
2022-10-10 15:38:45 -0300

    función filtrar

--- proyecto/pagina administracion/Proyecto.js
+++ proyecto/pagina administracion/Proyecto.js
@@ -5,8 +5,8 @@ let description = document.getElementById('descripcion');
 let url = document.getElementById('url');
 let id = document.getElementById('id');
 let fila = document.getElementById('datatable-tbody');
-let search = document.getElementById('search-movie');
-let filtrarbtn = document.getElementById('filtrarbtn');
+
+
 
 let pelicula = JSON.parse(localStorage.getItem('Peliculas')) || [];
 
@@ -98,4 +98,73 @@ function ReadFunction() {
 //     ReadFunction();
 //     window.location.reload()
 // }
+
+// let celda = document.getElementsByTagName('td');
+
+// inputbuscar.addEventListener('keyup', (e)=>{
+//     let texto = e.target.value
+
+//     let er = new RegExp(texto, 'i')
+
+//     for (let i = 0; i < celda.length; i++) {
+//         let valor = celda[i]
+
+
+//         if (er.test(valor.innerText)) {
+// valor.classList.remove("ocultar")
+//         } else {
+
+//             valor.classList.add("ocultar")
+//         }
+//     }
+// })
+
+// document.addEventListener("keyup", e=>{
+//     if(e.target.matches("#search-movie")){
+
+
+
+//         document.querySelectorAll(".articulo").forEach(titulo =>{
+//             titulo.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+//             ?titulo.classList.remove("ocultar")
+//             :titulo.classList.add("ocultar")
+
+//         })
+// }
+
+// })
+
+const inputbuscar = document.querySelector('#search-movie');
+let btn = document.getElementById('btn');
+const filtrar = () => {
+fila.innerHTML = '';
+
+    const texto = inputbuscar.value.toLowerCase();
+    for (let peli of pelicula) {
+        let titulo = peli.title.toLowerCase();
+        if(titulo.indexOf(texto) !== -1) {
+fila.innerHTML += `<tr>
+           
+<td>${peli.id}</td>
+<td>${peli.title}</td>
+<td>${peli.category}</td>
+<td>${peli.description}</td>
+<td>${peli.url}</td>
+<td><button class="btn btn-secondary" onclick="deleteMovies(${peli.id})">delete</button></td>
+
+
+
+</tr>`
+        }
+    }
+    if(fila.innerHTML === ''){
+        File.innerHTML += `td></td>
+        <td>Película no encontrada  </td>
+        <td></td>
+        <td></td>
+        <td></td>`
+    }
+}
+
+inputbuscar.addEventListener('keyup', filtrar);
 
