23e8cf4610be74dbbcc35cc8ce12619311654ae5
guillermofarhat <gacunafarhat.empresas@gmail.com>
2022-10-11 11:55:08 -0300

    trabajo matutino con juli

--- PROYECTO 2  JS SITIO DE PELICULAS/app.js
+++ PROYECTO 2  JS SITIO DE PELICULAS/app.js
@@ -22,27 +22,29 @@ let category = document.getElementById('categoria');
 let description = document.getElementById('descripcion');
 let url = document.getElementById('url');
 let id = document.getElementById('id');
+let image = document.getElementById ("image");
+let carousel =document.getElementById ("carousel__lista");
 // let fila = document.getElementById('datatable-tbody');
-let search = document.getElementById('search-movie');
-let filtrarbtn = document.getElementById('filtrarbtn');
+// let search = document.getElementById('search-movie');
+// let filtrarbtn = document.getElementById('filtrarbtn');
 
 
 let pelicula = JSON.parse(localStorage.getItem('Peliculas')) || [];
 // CREAR
 
 
-createFunction();
+// createFunction();
 buttonSave.addEventListener('click', () => {
 
    
-    if (title.value != "" && description.value != "" && category.value != "") {
+    if (title.value != "" && description.value != "" && image.value != "") {
         pelicula.push({
 
-            // id: Math.round(Math.random() * 100000),
             title: title.value,
             category: categoria.value,
-            // description: descripcion.value,
+            description: descripcion.value,
             url: url.value,
+            image: image.value,
 
         })
         localStorage.setItem('Peliculas', JSON.stringify(pelicula));
@@ -50,10 +52,12 @@ buttonSave.addEventListener('click', () => {
         category.value = "";
         description.value = "";
         url.value = "";
+        image.value = "";
 
         ReadFunction();
+        console.log(pelicula)
     } else {
-        alert("Campos vacíos")
+        alert("Falta informacion para cargar nueva pelicula")
     }
 
 })
@@ -64,26 +68,23 @@ function ReadFunction() {
 
     if (pelicula != null) {
         for (let index = 0; index < pelicula.length; index++) {
-            arrayPeliculas.push(`
-            <tr>
-           
-            <td>${pelicula[index].id}</td>
-            <td>${pelicula[index].title}</td>
-            <td>${pelicula[index].category}</td>
-            <td>${pelicula[index].description}</td>
-            <td>${pelicula[index].url}</td>
-            <td><button class="btn btn-secondary" onclick="deleteMovies(${pelicula[index].id})">delete</button></td>
-            
-            
+            arrayPeliculas.push(`  
 
-        </tr>
-                    
-                        `)
-    
-        }
-
+            <div  class="carousel__elemento">
+            <img  alt="">${pelicula[index].image}
+            <a href="" id="link-trailer">${pelicula[index].url}</a>
+            <p class="text" id="title">${pelicula[index].title}</p>
+            <p class="text" id="descripcion">${pelicula[index].description}</p>
+            </div>
+            `)
+          console.log(pelicula[index].image)
+            
+          }
+          carousel__lista.innerHTML = arrayPeliculas
+          
     } else {
-        arrayPeliculas = [];
-        fila.innerHTML = arrayPeliculas
+      carousel__lista.innerHTML = arrayPeliculas
+      arrayPeliculas = [];
     }
 }
+ReadFunction() 
