cd013f27793f7d4d8c243a6d90d5556e1267e9cb
jzarzosa <majorminus4697@gmail.com>
2022-10-12 23:56:30 -0300

    categorización de películas y modal con info

--- proyecto/pagina administracion/Proyecto.js
+++ proyecto/pagina administracion/Proyecto.js
@@ -3,18 +3,21 @@ let title = document.getElementById('title');
 let category = document.getElementById('categoria');
 let description = document.getElementById('descripcion');
 let url = document.getElementById('url');
-let id = document.getElementById('id');
+
 let fila = document.getElementById('datatable-tbody');
+let carruselaction = document.getElementById('carruselaction');
+let carruselterror = document.getElementById('carruselterror');
+let carruselcomedia = document.getElementById('carruselcomedia');
 
 
 
 let pelicula = JSON.parse(localStorage.getItem('Peliculas')) || [];
-
+ReadCarrusel();
 ReadFunction();
 buttonSave.addEventListener('click', () => {
 
-   
-    if (title.value != "" && description.value != "" && category.value != "") {
+
+    if (title.value != "" && description.value != "") {
         pelicula.push({
 
             id: Math.round(Math.random() * 100000),
@@ -25,24 +28,32 @@ buttonSave.addEventListener('click', () => {
 
         })
         localStorage.setItem('Peliculas', JSON.stringify(pelicula));
+
         title.value = "";
-        category.value = "";
-        description.value = "";
+        categoria.value = "";
+        descripcion.value = "";
         url.value = "";
 
         ReadFunction();
     } else {
         alert("Campos vacíos")
     }
+    location.reload()
 
 })
 
 function ReadFunction() {
 
     let arrayPeliculas = [];
+    let arrayPeliculasaction = [];
+    let arrayPeliculasterror = [];
+    let arrayPeliculascomedia = [];
+
 
     if (pelicula != null) {
         for (let index = 0; index < pelicula.length; index++) {
+
+            //creo la tabla
             arrayPeliculas.push(`
             <tr>
            
@@ -50,7 +61,7 @@ function ReadFunction() {
             <td>${pelicula[index].title}</td>
             <td>${pelicula[index].category}</td>
             <td>${pelicula[index].description}</td>
-            <td>${pelicula[index].url}</td>
+            <td>true/false</td>
             <td><button class="btn btn-secondary" onclick="deleteMovies(${pelicula[index].id})">delete</button></td>
             
             
@@ -58,7 +69,9 @@ function ReadFunction() {
         </tr>
                     
                         `)
-                        fila.innerHTML = arrayPeliculas
+
+            fila.innerHTML = arrayPeliculas
+
         }
 
     } else {
@@ -68,19 +81,21 @@ function ReadFunction() {
 }
 
 
+
+
 //Delete
- const deleteMovies = (id) => {
-    
-     let deleteMovie = []
-     pelicula.map((item) => {
-         if (id != item.id) {
-             deleteMovie.push(item);
-         }
-     })
-     localStorage.setItem('Peliculas', JSON.stringify(deleteMovie));
-     location.reload()
-     ReadFunction();
- }
+const deleteMovies = (id) => {
+
+    let deleteMovie = []
+    pelicula.map((item) => {
+        if (id != item.id) {
+            deleteMovie.push(item);
+        }
+    })
+    localStorage.setItem('Peliculas', JSON.stringify(deleteMovie));
+    location.reload()
+    ReadFunction();
+}
 
 // function deleteMovie(index) {
 
@@ -137,19 +152,19 @@ function ReadFunction() {
 const inputbuscar = document.querySelector('#search-movie');
 let btn = document.getElementById('btn');
 const filtrar = () => {
-fila.innerHTML = '';
+    fila.innerHTML = '';
 
     const texto = inputbuscar.value.toLowerCase();
     for (let peli of pelicula) {
         let titulo = peli.title.toLowerCase();
-        if(titulo.indexOf(texto) !== -1) {
-fila.innerHTML += `<tr>
+        if (titulo.indexOf(texto) !== -1) {
+            fila.innerHTML += `<tr>
            
 <td>${peli.id}</td>
 <td>${peli.title}</td>
 <td>${peli.category}</td>
 <td>${peli.description}</td>
-<td>${peli.url}</td>
+<td>true/false</td>
 <td><button class="btn btn-secondary" onclick="deleteMovies(${peli.id})">delete</button></td>
 
 
@@ -157,7 +172,7 @@ fila.innerHTML += `<tr>
 </tr>`
         }
     }
-    if(fila.innerHTML === ''){
+    if (fila.innerHTML === '') {
         File.innerHTML += `td></td>
         <td>Película no encontrada  </td>
         <td></td>
@@ -167,4 +182,136 @@ fila.innerHTML += `<tr>
 }
 
 inputbuscar.addEventListener('keyup', filtrar);
+
+
+
+
+
+
+
+function ReadCarrusel() {
+
+    let arrayPeliculasaction = [];
+    let arrayPeliculasterror = [];
+    let arrayPeliculascomedia = [];
+
+
+    if (pelicula != null) {
+        for (let index = 0; index < pelicula.length; index++) {
+            //creo carrusel action
+            if (pelicula[index].category == "action") {
+                arrayPeliculasaction.push(`
+
+    <div class="tarjeta">
+    <img src="${pelicula[index].url}" width="200px" height="300px" type="button" class="btn" data-bs-target="#exampleModalaction" data-bs-toggle="modal">
+
+
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalaction" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+          <h3>${pelicula[index].title}</h3>
+          <h6>${pelicula[index].description}</h6>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="cuerpocard"></div>
+    </div>
+                    
+                        `)
+                        carruselaction.innerHTML = arrayPeliculasaction
+            }
+
+            //creo carrusel terror
+            if (pelicula[index].category == "terror") {
+                arrayPeliculasterror.push(`
+
+                <div class="tarjeta">
+                <img src="${pelicula[index].url}" width="200px" height="300px" type="button" class="btn" data-bs-target="#exampleModalterror" data-bs-toggle="modal">
+            
+            
+                
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModalterror" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                      <h3>${pelicula[index].title}</h3>
+                      <h6>${pelicula[index].description}</h6>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            
+            
+                <div class="cuerpocard"></div>
+                </div>
+            
+                `)
+                carruselterror.innerHTML = arrayPeliculasterror
+
+            }
+
+            //creo carrusel comedia
+            if (pelicula[index].category == "comedia") {
+                arrayPeliculascomedia.push(`
+
+                <div class="tarjeta">
+                <img src="${pelicula[index].url}" width="200px" height="300px" type="button" class="btn" data-bs-target="#exampleModalcomedia" data-bs-toggle="modal">
+            
+            
+                
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModalcomedia" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                      <h3>${pelicula[index].title}</h3>
+                      <h6>${pelicula[index].description}</h6>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            
+            
+                <div class="cuerpocard"></div>
+                </div>
+            
+                `)
+                carruselcomedia.innerHTML = arrayPeliculascomedia
+
+            }
+        }
+
+    }
+}
 
